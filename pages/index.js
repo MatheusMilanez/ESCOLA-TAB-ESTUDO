85d57ccb87431a6e3f71f8067202d34f345050fb
Matheus Milanez <99884832+MatheusMilanez@users.noreply.github.com>
2024-07-12 14:43:25 +0000

    alterado texto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-  return <h1>♥Isabela, eu amo você. se você me ama, dá uma risadinha!♥</h1>;
+  return (
+    <h1>♥Isabela Suzana, eu amo você. se você me ama, dá uma risadinha!♥</h1>
+  );
 }
 
 export default Home;
